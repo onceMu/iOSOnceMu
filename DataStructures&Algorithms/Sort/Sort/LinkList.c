f78543431b3feb84836ee4ffa0e21a444069dd87
pengbin <pengbin@onehome.me>
2018-07-18 18:50:57 +0800

    链表算法

--- DataStructures&Algorithms/Sort/Sort/LinkList.c
+++ DataStructures&Algorithms/Sort/Sort/LinkList.c
@@ -79,3 +79,72 @@ void printMiddleLinkList(struct Node *head) {
     }
 }
 
+//对于给定的链表，指定data 为key，删除最后一个出现的指定key,如果没有包含key，返回invalid data，
+//要求O(n)
+//如1 ->3 ->4 ->5 -> 6 ->7 ->8 ->9 ->3 ->1 -> null
+//删除3 1 ->3 ->4 ->5 -> 6 ->7 ->8 ->9  ->1 -> null
+//思路
+void deleteLastKey(struct Node *head,int key) {
+    struct Node *temp = head;
+    struct Node *ptr = NULL;
+    //从头开始遍历链表，如果key存在，则将temp 赋值给 ptr
+    while (temp) {
+        if (temp -> data == key) {
+            //ptr 会指向最后一个 == key 的节点
+            ptr = temp;
+        }
+        temp = temp -> next;
+    }
+    //如果ptr 不为空并且 ptr的下一个节点也不为空，即ptr 不是最后一个节点
+    if (ptr != NULL && ptr ->next != NULL) {
+        temp = head;
+        while (temp -> next != ptr) {
+            temp = temp -> next;
+        }
+        temp -> next = NULL;
+    }
+    if (ptr != NULL && ptr == NULL) {
+        ptr -> data = ptr ->next ->data;
+        temp = ptr ->next;
+        ptr ->next = ptr ->next ->next;
+        free(temp);
+    }
+}
+
+void displyDeletedList(struct Node *head) {
+    struct Node* temp = head;
+    if (head == NULL) {
+        printf("NULL\n");
+        return;
+    }
+    while (temp != NULL) {
+        printf("%d --> ", temp->data);
+        temp = temp->next;
+    }
+    printf("NULL\n");
+}
+
+//删除链表给定位置的节点
+void deletePositionNode(struct Node *head,int position) {
+    if (head == NULL) {
+        return;
+    }
+    struct Node *temp = head;
+    if (position == 0) {
+        head = temp ->next;
+        free(temp);
+        return;
+    }
+    for (int i =0; i<position -1; i++) {
+        temp = temp ->next;
+    }
+    //如果越界
+    if (temp == NULL || temp -> next == NULL) {
+        return;
+    }
+    struct Node *next = temp ->next->next;
+    free(temp ->next);
+    temp ->next = next;
+}
+
+
