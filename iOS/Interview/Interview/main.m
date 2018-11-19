57399a21de5fee6576f176c4ec54e17cd10d4d55
pengbin <pengbin@onehome.me>
2018-11-19 18:28:52 +0800

    学习

--- iOS/Interview/Interview/main.m
+++ iOS/Interview/Interview/main.m
@@ -11,34 +11,19 @@
 #import <objc/runtime.h>
 
 
-@interface Student : NSObject {
-    @public
-    int _no;
-    int _age;
-}
-
-@end
-
-@implementation Student
-
-@end
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
-        NSObject *object = [[NSObject alloc]init];
-        
-        Student *stu = [[Student alloc]init];
-        
-        //获取object类实例对象成员变量的大小
-        NSLog(@"%zd",class_getInstanceSize([Student class]));
-        
-        
-        //获取object指针指向内存的大小
-        NSLog(@"%zd",malloc_size((__bridge const void *)stu));
-        
-        
+
+        id __strong obj = [[NSObject alloc]init];
+        __weak id object = obj;
         
     }
     return 0;
 }
+
+
+//id __attribute__((objc_ownership(strong))) obj = ((NSObject *(*)(id, SEL))(void *)objc_msgSend)((id)((NSObject *(*)(id, SEL))(void *)objc_msgSend)((id)objc_getClass("NSObject"), sel_registerName("alloc")), sel_registerName("init"));
+
+
